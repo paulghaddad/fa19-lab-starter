fc707489a2e767d15580942518c5195f9e7ad545
Paul Haddad <paulh16@gmail.com>
2019-12-03 08:25:17 -0500

    Finish Lab 01

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-.PHONY: lab
-lab:
+.PHONY: docker
+docker:
 	docker run -it --rm --mount type=bind,src=/Users/paulh16/Dropbox/Bradfield/architecture/fa19-lab-starter,dst=/cs61c-labs bradfield-architecture
--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,25 @@
 #include <stddef.h>
 #include "ll_cycle.h"
+#include <stdio.h>
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if (head == NULL)
+      return 0;
+
+    struct node *hare = head;
+    struct node *tortoise = head;
+
+    while ((hare = hare->next) != 0) {
+      if ((hare = hare->next) == 0) {
+        return 0;
+      }
+
+      tortoise = tortoise->next;
+
+      if (hare == tortoise) {
+        return 1;
+      }
+    }
+
     return 0;
 }
